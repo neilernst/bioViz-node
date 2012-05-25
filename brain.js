36e941234391fc0f2372aed1efba0a3514a780d9
Neil Ernst <neil@neilernst.net>
2012-05-25 16:25:19 -0700

    renamed for clarity

---
+++ brain.js
@@ -0,0 +1,14 @@
+function Highlight(evt) 
+{ 
+	evt.target.setAttribute("fill", "blue");
+} 
+
+function Unhighlight(evt) 
+{ 
+	evt.target.setAttribute("fill", "white"); 
+} 
+	    
+$(document).ready(function() {
+
+
+});
