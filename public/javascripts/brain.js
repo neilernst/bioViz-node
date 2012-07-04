d324051afa286c0d451cd8128926f9de9f318646
Neil Ernst <neil@neilernst.net>
2012-07-04 13:37:22 -0700

    svg work with brain and jquery

--- public/javascripts/brain.js
+++ public/javascripts/brain.js
@@ -1,14 +1,13 @@
-function Highlight(evt) 
-{ 
-	evt.target.setAttribute("fill", "blue");
-} 
-
-function Unhighlight(evt) 
-{ 
-	evt.target.setAttribute("fill", "white"); 
-} 
-	    
-$(document).ready(function() {
-
-
+$(document).ready(function(){
+	$('#brain-img').svg({onLoad: drawMixture});
 });
+
+function drawMixture(svg) {
+	svg.configure({width: '540px', height: '400px'});
+	svg.load('images/brain-map.svg');
+	nb = svg.getElementById('BA5');
+	$('#BA5').attr('fill','green');
+	na = $('#na-group').hide();//$('#brain-svg').hide();
+	alert($.svg.isSVGElem(nb)	);
+	//$('#BA7').setAttribute("fill", "red");
+}
